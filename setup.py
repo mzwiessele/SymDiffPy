2332bb0949702326db074682a531589e819efc86
mzwiessele <ibinbei@gmail.com>
2015-03-03 11:56:53 +0100

    [add] src folder

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+# Version number
+version = '0.0.1'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name = 'SymDiffPy',
+      version = version,
+      description = ("SymDiffPy"),
+      license = "BSD 3-clause",
+      keywords = "machine-learning gaussian-processes kernels",
+      packages = ["SymDiffPy.models",
+                  "SymDiffPy.kernels",
+                  ],
+      package_dir={'SymDiffPy': 'SymDiffPy'},
+      install_requires=['numpy>=1.7', 'scipy>=0.12', 'theano>=0.6'],
+      )
